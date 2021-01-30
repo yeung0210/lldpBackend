b9a7d24fa91741e8fefd3d1838d834064a6f035d
Sammi Yeung <sammiyc0210@gmail.com>
2021-01-30 23:23:18 +0800

    Token Authentication function

--- controllers/user.js
+++ controllers/user.js
@@ -52,8 +52,17 @@ module.exports = {
             token : accessToken
         };
         res.send(common.response(200, '成功登入', data))
+    }, 
+    authenticate: (req, res, nex) => {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+        if (token == null) { return res.sendStatus(401) }
 
-        
+        jwt.verify(token, process.env.SECRET, (err, user) => {
+            if (err) { return res.sendStatus(403) }
+            req.user = user
+            next()
+        })
     }
 }
 
