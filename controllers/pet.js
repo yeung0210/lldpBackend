c82540440956235c2ff9bea88822ab919be49625
Sammi Yeung <sammiyc0210@gmail.com>
2021-03-29 22:32:17 +0800

    Get userid from token

--- controllers/pet.js
+++ controllers/pet.js
@@ -20,9 +20,11 @@ module.exports = {
         })
     },
     getByUserId: (req, res) => {
-        const user_id = req.body.user_id
-        Clinic.find({ 'user_id': user_id }, function (err, docs) {
+        userController.authenticate(req, res, function() {
+            const user_id = req.user.user_id
+        Pet.find({ 'user_id': user_id }, function (err, docs) {
             res.send(common.response(200, 'Pet' + district, docs))
         });
+        })
     },
 }
